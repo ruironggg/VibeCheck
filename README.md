bdce6e6fcd24d3dd2b76ff46800c7c7ab7fde044
Justin Tan <tan_yan_feng@u.nus.edu>
2023-01-21 17:29:26 -0500

    Switched to conda while allowing pip

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # VibeCheck
 
-run using `flask run`
-It runs on port 5000
+## Updating local conda environment.yml
+
+1. `conda env export --no-build -f environment.yml`, this way environment.yml works for any Operating System
+
+## Updating requirements.txt
+1. `pip freeze > requirements.txt`
+
+## Create venv using pip
+````
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+````
+
+
+## Create conda enviroment
+`conda env create -f environment.yml`
+
+
+## How to run
+run using `flask run`, it runs on port 5000
+
--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import json
 import time
 from flask import Flask, request
+# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
@@ -23,4 +24,43 @@ def train_model():
     else:
         return json.dumps({'error_message': 'Content-Type not supported!'})
 
+
 # Add your APIs below
+#
+# @app.route('/test')
+# def test():
+#     # Initialize the sentiment analyzer
+#     analyzer = SentimentIntensityAnalyzer()
+#
+#     # The text to be analyzed
+#     text = "I am feeling great today! Everything is going well and I am very confident in my abilities."
+#
+#     # Get the sentiment scores
+#     scores = analyzer.polarity_scores(text)
+#     print(scores)
+#
+#     # Get the positive sentiment score
+#     positive_sentiment = scores['pos']
+#
+#     # Get the compound sentiment score
+#     confidence = scores['compound']
+#
+#     # Get the positive sentiment score
+#     happy_sentiment = scores['pos']
+#
+#     print(f"The positive sentiment of the text is: {positive_sentiment}")
+#     print(f"The confidence of the person is: {confidence}")
+#     print(f"The happy sentiment of the text is: {happy_sentiment}")
+#
+#     nervous_bool = scores['neg'] > scores['pos']
+#
+#     # Check if the person is feeling unsure
+#     unsure_bool = confidence < 0.1
+#
+#     # Check if the person is feeling happy
+#     unhappy_bool = scores['pos'] > scores['neg']
+#
+#     # Check if the person is feeling proud
+#     proud_bool = scores['pos'] > 0.6
+#
+#     return json.dumps({'agent': "agent"})
